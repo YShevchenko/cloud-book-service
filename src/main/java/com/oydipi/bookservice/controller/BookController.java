d6efaf18dbd6cfa162c35110867e96dbdfd06b37
Yaroslav Shevchenko <yaroslav.shevchenko@globallogic.com>
2019-02-07 00:01:56 +0200

    add logging

--- src/main/java/com/oydipi/bookservice/controller/BookController.java
+++ src/main/java/com/oydipi/bookservice/controller/BookController.java
@@ -1,12 +1,14 @@
 package com.oydipi.bookservice.controller;
 
 import com.oydipi.bookservice.model.Book;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -20,16 +22,19 @@ public class BookController {
 
     @GetMapping
     public List<Book> getBooks() {
+        log.info("All books fetched");
         return books;
     }
 
     @GetMapping("/{bookId}")
     public Book getBook(@PathVariable String bookId) {
+        log.info("book id {} retrieved", bookId);
         return books.stream().filter(b -> b.getId().equals(bookId)).findFirst().orElse(null);
     }
 
     @PostMapping
     public Book addBook(@RequestBody Book book) {
+        log.info("book added {}", book);
         return books.add(book) ? book : null;
     }
 }
